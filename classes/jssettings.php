0fbfc81edc5e27a17e6467943e7850bcf9e8f3a4
Lorenzo Pisani <zeelot3k@gmail.com>
2010-04-06 17:21:55 -0700

    caching JSSettings file

--- classes/jssettings.php
+++ classes/jssettings.php
@@ -53,6 +53,8 @@ class JSSettings {
 
 	public function script_content()
 	{
+		$config = Kohana::config('jssettings');
+
 		$this->read_routes();
 
 		$view = View::factory('jssettings/jssettings.js')
@@ -60,11 +62,37 @@ class JSSettings {
 			->set('config', $this->_config)
 			->set('properties', $this->_properties);
 
+		if ($config->cache === TRUE)
+		{
+			// Cache this file for future requests
+			file_put_contents($config->cache_file, $view->render());
+			Kohana::$log->add(Kohana::INFO, 'Cached JSSettings to '.$config->cache_file);
+		}
+
 		return $view;
 	}
 
 	public function script()
 	{
+		$config = Kohana::config('jssettings');
+
+		if ($config->cache === TRUE AND file_exists($config->cache_file))
+		{
+			$creation = filemtime($config->cache_file);
+			$expiration = $creation + ($config->cache_lifetime);
+			if (time() > $expiration)
+			{
+				// Cache Expired (force the application to create a new one)
+				unlink($config->cache_file);
+				Kohana::$log->add(Kohana::INFO, 'Deleted Cached JSSettings '.$config->cache_file);
+			}
+			else
+			{
+				Kohana::$log->add(Kohana::INFO, 'Using Cached JSSettings '.$config->cache_uri);
+				// Include the cached js file
+				return HTML::script($config->cache_uri);
+			}
+		}
 		return HTML::script(Route::get('jssettings')->uri());
 	}
 }
--- config/jssettings.php
+++ config/jssettings.php
@@ -2,5 +2,8 @@
 
 return array
 (
-
+	'cache'          => TRUE,
+	'cache_file'     => DOCROOT.'media/js/jssettings.js',
+	'cache_uri'      => 'media/js/jssettings.js',
+	'cache_lifetime' => '600',
 );
